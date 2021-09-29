36d13f1a7875213df04d290207f3564af7d8d986
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2021-09-29 16:33:36 -0500

    use server-side rendering

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 import Layout from '@/components/Layout'
+import { API_URL } from '@/config/index'
 
-export default function Home() {
+export default function Home({events}) {
+  console.log(events)
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming events</h1>
     </Layout>
   )
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  return {
+    props: { events },
+  }
 }
